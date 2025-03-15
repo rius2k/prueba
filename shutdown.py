88874eabe44b8b9d9fb97cacd77ad93e74f203e5
rius2k <f.unnys1ck@outlook.com>
2025-03-15 01:56:26 -0600

    Ok

--- shutdown.py
+++ shutdown.py
@@ -5,12 +5,10 @@ boton_pin = 17
 GPIO.setup(boton_pin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 try:
     while True:
-        # Leer el estado del botón (si está presionado o no)
         if GPIO.input(boton_pin) == GPIO.HIGH:
-            print("¡Botón presionado! Ejecutando el comando...")
-            # Ejecutar el comando deseado, por ejemplo, apagar
-            os.system('sudo shutdown now')
-            time.sleep(0.5)  # Para evitar múltiples detecciones al mantener presionado
+            print("Botón presionado")
+            #os.system('sudo shutdown now')
+            time.sleep(0.5)
         time.sleep(0.1)
 
 except KeyboardInterrupt:
